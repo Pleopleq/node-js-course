18f3edf59822c313daf58c65fa29f78eb2c1fc07
Felix Anducho <felix.anducho74@gmail.com>
2023-01-26 19:22:38 -0600

    extract routes to his own file

---
+++ chapter-01/create-server/routes.js
@@ -0,0 +1,50 @@
+const fs = require("fs")
+
+function requestHandler(req, res) {
+    const url = req.url
+    const method = req.method
+
+    if(url === "/") {
+        res.write('<html>')
+        res.write('<head><title>FIRST PAGE LES GOOO WHERES THE MONEY</title></head>')
+        res.write(`<body>
+        <h1>HELLO FROM MY NODE JS SERVER</h1>
+            <form action="/message" method="post">
+                <input type="text" name="message">
+                <button type="submit">Submit</button>
+            </form>
+        </body>`)
+        res.write('</html>')
+        return res.end() 
+    }
+
+    if(url === "/message" && method === "POST") {
+        const body = []
+
+        req.on('data', function(chunk) {
+            body.push(chunk)
+        })
+
+        req.on('end', function() {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split("=")[1]
+            fs.writeFile('message.txt', message, function(error) {
+                if (error) {
+                    return console.log(error)
+                }
+
+                res.writeHead(302, { Location: "/"})
+                return res.end()
+            })
+        })
+    }
+
+    res.setHeader('Content-Type', "text/html");
+    res.write('<html>')
+    res.write('<head><title>FIRST PAGE LES GOOO WHERES THE MONEY</title></head>')
+    res.write('<body><h1>OTHER PAGE</h1></body>')
+    res.write('</html>')
+    res.end()    
+}
+
+module.exports = requestHandler
