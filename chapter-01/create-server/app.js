b702e1f14ce365e186f42ea1520afeeb9f51d95d
Felix Anducho <felix.anducho74@gmail.com>
2023-01-30 18:38:54 -0600

    Working with middlewares

--- chapter-01/create-server/app.js
+++ chapter-01/create-server/app.js
@@ -2,15 +2,14 @@ const express = require("express")
 
 const app = express()
 
-app.use((req, res, next) => {
+app.use("/add-product" ,(req, res, next) => {
     console.log("MIDDLEWARE TRIGGERED")
-    next()
+    res.send("<h1>ADD PRODUCT PAGE</h1>")
 })
 
 app.use((req, res, next) => {
     console.log("MIDDLEWARE TRIGGERED AGAEN")
     res.send("<h1>USING EXPRESS</h1>")
-    next()
 })
 
 app.listen(3000)
