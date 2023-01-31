bf40ac64e95f203500d37cc6e202945667159000
Felix Anducho <felix.anducho74@gmail.com>
2023-01-30 19:11:37 -0600

    adding a 404 error page

--- chapter-01/create-server/app.js
+++ chapter-01/create-server/app.js
@@ -9,4 +9,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(adminRouter)
 app.use(shopRouter)
 
+app.use(function(req,res, next) {
+    res.status(404).send("<h1>ERROR 404 - PAGE NOT FOUND</h1>")
+})
+
 app.listen(3000)
