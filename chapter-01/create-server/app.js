8e1159ba6434d8e37e9b891a474ac693a2a28825
Felix Anducho <felix.anducho74@gmail.com>
2023-01-30 19:08:54 -0600

    using express router

--- chapter-01/create-server/app.js
+++ chapter-01/create-server/app.js
@@ -1,24 +1,12 @@
 const bodyParser = require("body-parser")
 const express = require("express")
+const adminRouter = require("./routes/admin")
+const shopRouter = require("./routes/shop")
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.use("/add-product" ,(req, res, next) => {
-    res.send(`
-    <form action="/product" method="POST">
-        <input type="text" name="title" id="title">
-        <button type="submit">Send</button>
-    </form>`)
-})
-
-app.post("/product", (req, res, next) => {
-    console.log(req.body)
-    res.redirect("/")
-})
-
-app.use((req, res, next) => {
-    res.send("<h1>USING EXPRESS</h1>")
-})
+app.use(adminRouter)
+app.use(shopRouter)
 
 app.listen(3000)
