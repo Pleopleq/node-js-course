e039755909467ae391f11a9fc5aca05406410a65
Felix Anducho <felix.anducho74@gmail.com>
2023-02-08 18:52:48 -0600

    Delete items from admin products and cart

--- chapter-09/00-starting-setup/models/product.js
+++ chapter-09/00-starting-setup/models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart')
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -54,6 +56,18 @@ module.exports = class Product {
     getProductsFromFile(products => {
       const productFound = products.find(product => product.id === id)
       cb(productFound)
+    })
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id)
+      const updatedProducts = products.filter(product => product.id !== id)
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err) {
+          Cart.deleteProduct(id, product.price)
+        }
+      })
     })
   }
 };
